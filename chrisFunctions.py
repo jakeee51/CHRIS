230280fc911032c1e426a102962bdccd725b8c0e
Wadi <hwadi@LAMU02DQ8D0MD6R.uhc.com>
2021-08-05 13:38:19 -0400

    added clone, pull, java and webpage

---
+++ chrisFunctions.py
@@ -0,0 +1,20 @@
+import requests
+import json
+import argparse
+import git
+import os
+import webbrowser
+import jdk
+
+def cloneRepo(githubRepoUrl,repoName):
+    git.Repo.clone_from(githubRepoUrl,repoName)
+#https://github.optum.com/pes/NDBProviderSearchV10-war.git
+def pullRepo(repoName,branchName):
+    g = git.Git(repoName)
+    g.pull('origin',branchName)
+
+def openPage(pageUrl):
+    webbrowser.open(pageUrl)
+
+def installJava(versionNum):
+    jdk.install(str(versionNum))
