f555ae1e625582ebb5b1bee3954b9af1b95f10d0
David Jake Morfe <jakemorfe@gmail.com>
2021-08-05 18:14:01 -0400

    Init

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+echo "Installing dependencies..."
 pip3 install -r requirments.txt
-brew install portaudio && pip3 install pyaudio && brew install flac
 sudo su
 chmod 777 /usr/local/share/zsh
 chmod 777 /usr/local/share/zsh/site-functions
+exit
+brew install portaudio && pip3 install pyaudio && brew install flac
 echo "Execute chrisTests.py from terminal then *allow* microphone permission"
